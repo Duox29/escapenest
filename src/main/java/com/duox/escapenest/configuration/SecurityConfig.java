93b485e632ac341fe03ba70d8021c2d72635bce2
Duox29 <96653304+Duox29@users.noreply.github.com>
2025-04-11 00:08:49 +0700

    Update jwtdecode + store invalid token in redis

--- src/main/java/com/duox/escapenest/configuration/SecurityConfig.java
+++ src/main/java/com/duox/escapenest/configuration/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.duox.escapenest.configuration;
 
+import com.duox.escapenest.constant.ResultCode;
+import com.duox.escapenest.dto.response.valueObject.ResultMessage;
+import com.duox.escapenest.util.ResultUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -21,7 +28,7 @@ public class SecurityConfig {
             "/auth/login"
     };
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, CustomJwtDecoder customJwtDecoder, JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception{
         httpSecurity
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
@@ -36,10 +43,30 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT).permitAll()
                         .requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINT).permitAll()
-                        .requestMatchers((HttpMethod.PUT,PUBLIC_ENDPOINT).permitAll()
+                        .requestMatchers(HttpMethod.PUT,PUBLIC_ENDPOINT).permitAll()
                         .anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder())))
+                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(customJwtDecoder)
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                                .authenticationEntryPoint((request, response, authException) -> {
+                                    ResultCode resultCode = ResultCode.UNAUTHENTICATED;
+                                    response.setStatus(resultCode.getHttpStatus().value());
+                                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                                    ResultMessage<?> resultMessage = ResultUtil.error(resultCode);
+                                    ObjectMapper objectMapper = new ObjectMapper();
+                                    response.getWriter().write(objectMapper.writeValueAsString(resultMessage));
+                                    response.flushBuffer();
+                                }));
+        return httpSecurity.build();
     }
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
 }
